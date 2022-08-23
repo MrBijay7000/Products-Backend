2d6bf82824a0751ba028cbe6dd9be13ad346a12c
Bijay Shrestha <shresthabzay95@gmail.com>
2022-08-23 19:48:34 +0545

    Get product with catgeory name

--- controllers/product.js
+++ controllers/product.js
@@ -8,6 +8,7 @@ exports.createProduct = (req, res, next) => {
     quantity: req.body.productQuantity,
     category: req.body.category,
   });
+
   product.save().then((createdProduct) => {
     console.log({ createdProduct });
     const obj = {
@@ -23,92 +24,57 @@ exports.createProduct = (req, res, next) => {
       createdProduct: obj,
     });
   });
+
+  // const createdProduct = await product.save();
+  // const obj = {
+  //   name: createdProduct.name,
+  //   price: createdProduct.price,
+  //   quantity: createdProduct.quantity,
+  //   id: createdProduct._id,
+  //   categoryId: createdProduct.category,
+  //   _version: createdProduct.__v,
+  // };
+  // res.json({
+  //   message: "Product Created",
+  //   createdProduct: obj,
+  // });
 };
 
-// const getCategoryById = async (categoryId) => {
-//   const category = await Category.findById(categoryId);
-//   return category;
+// exports.getProduct = (req, res, next) => {
+//   Product.find().then((response) => {
+//     const arr = response.map((product) => {
+//       console.log({ product });
+//       return {
+//         name: product.name,
+//         price: product.price,
+//         quantity: product.quantity,
+//         id: product._id,
+//         category: product.category,
+//         _version: product.__v,
+//       };
+//     });
+//     res.json(arr);
+//   });
 // };
 
 exports.getProduct = async (req, res, next) => {
-  const arr = [];
-  Product.find().then((response) => {
-    const newArr = response.map(async (product) => {
-      const ajay = Category.findById(product.category).then((category) => {
-        const obj = {
-          name: product.name,
-          price: product.price,
-          quantity: product.quantity,
-          id: product._id,
-          category: {
-            name: category.name,
-            id: category._id,
-          },
-          _version: product.__v,
-        };
-        return obj;
-        console.log({ obj });
-      });
-      console.log({ ajay });
-    });
-    console.log({ newArr });
-    res.json(newArr);
-  });
+  const products = await Product.find();
+  const arr = await Promise.all(
+    products.map(async (product) => {
+      const category = await Category.findById(product.category);
+      const obj = {
+        name: product.name,
+        price: product.price,
+        quantity: product.quantity,
+        id: product._id,
+        categoryName: category.name,
+        _version: product.__v,
+      };
+      return obj;
+    })
+  );
+  res.json(arr);
 };
-
-// exports.getProduct = (req, res, next) => {
-//   Product.find().then((resposne) => {
-//     const arr = resposne.map((product) => {
-//       console.log({ product });
-//       let obj = {};
-
-//       return Category.findById(product.category).then((category) => {
-//         obj = {
-//           name: category.name,
-//           id: category._id,
-//         };
-
-//         return {
-//           name: product.name,
-//           price: product.price,
-//           quantity: product.quantity,
-//           id: product._id,
-//           category: obj,
-//           _version: product.__v,
-//         };
-//       });
-//     });
-//     console.log({ arr });
-//     res.json(arr);
-//   });
-// };
-
-// exports.getProduct = (req, res, next) => {
-//   Product.find().then((resposne) => {
-//     const arr = resposne.map((product) => {
-//       console.log({ product });
-//       let obj = {};
-
-//       return Category.findById(product.category).then((category) => {
-//         obj = {
-//           name: category.name,
-//           id: category._id,
-//         };
-
-//         return {
-//           name: product.name,
-//           price: product.price,
-//           quantity: product.quantity,
-//           id: product._id,
-//           category: obj,
-//           _version: product.__v,
-//         };
-//       });
-//     });
-//     console.log({ arr });
-//     res.json(arr);
-//   });
-// };
 
 exports.deleteProduct = (req, res, next) => {
   Product.deleteOne({ _id: req.params.id }).then((result) => {
